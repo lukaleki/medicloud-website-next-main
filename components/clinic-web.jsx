b5409b95a348b2982c169cd59692e4985ea59c38
lukaleki <lekishvililuka76@gmail.com>
2023-08-02 22:18:32 +0400

    responsive update

--- components/clinic-web.jsx
+++ components/clinic-web.jsx
@@ -9,7 +9,7 @@ function ClinicWeb() {
       <div className="reg-left">
         <h1>კლინიკების ვებ აპლიკაცია</h1>
         <p>დაარეგისტრირეთ თქვენი კლინიკა აპლიკაცია მედიქლაუდში და გახადეთ თქვენი სამედიცინო სერვისები პაციენტებისთვის უფრო კომფორტული და ხელმისაწვდომი.</p>
-        <button className='reg-btn'>კლინიკების რეგისტრაცია <Image src={diagonalArrow} width='14' height='14' alt='diagonal-arrow'/></button>
+        <button className='web-btn'>საიტზე გადასვლა<Image src={diagonalArrow} width='14' height='14' alt='diagonal-arrow'/></button>
       </div>
       <Image className='clinic-img' src={clinicImg} width='756' height='431' alt='reg-img'/>
     </section>
