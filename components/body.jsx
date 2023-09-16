1c105c93ef62827051a09dafaadef7bedf813324
lukaleki <lekishvililuka76@gmail.com>
2023-09-16 20:40:08 +0400

    release

--- components/body.jsx
+++ components/body.jsx
@@ -13,15 +13,35 @@ import Questions from "../components/questions";
 import Blog from "../components/blog";
 import Footer from "../components/footer";
 import { useTheme } from "../pages/themeContext";
+import Image from "next/image";
+import diagonalArrow from "../public/diagonal-arrow.png"
+import sunIcon from "../public/sun.png"
+import moonIcon from "../public/moon.png"
 
 function Body() {
   const { darkTheme, toggleTheme } = useTheme();
   const darkThemeClass = darkTheme ? "dark-theme" : "";
-  console.log(darkTheme);
 
   return (
     <section className={`body-container ${darkThemeClass}`}>
       <div className="cosmetic-rectangle"></div>
+      <button className="theme-change">
+        <Image
+          className="theme-change-arrow"
+          src={diagonalArrow}
+          width="14"
+          height="14"
+          alt="theme-change-arrow"
+        />
+        <Image
+          onClick={toggleTheme}
+          className="sun-icon"
+          src={darkTheme ? moonIcon : sunIcon}
+          width="25"
+          height="25"
+          alt="sun-icon"
+        />
+      </button>
       <Nav />
       <ClinicRegistration />
       <MedPartner />
